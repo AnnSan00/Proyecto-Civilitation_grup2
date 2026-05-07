81409f5210a6a80637cb0da5fce28b7c552c79c5
Anna121104 <119697202+AnnSan00@users.noreply.github.com>
2026-05-07 19:10:31 +0200

    Variables militarUnit

--- Interfaces/MilitaryUnit.java
+++ Interfaces/MilitaryUnit.java
@@ -1,6 +1,15 @@
 package Interfaces;
 public interface MilitaryUnit {
-
-
-    
+    int attack();
+    void takeDamage(int receivedDamage);
+    int getActualArmor();
+    int getFoodCost();
+    int getWoodCost();
+    int getIronCost();
+    int getManaCost();
+    int getChanceGeneratingWaste();
+    int getChanceAttackAgain();
+    void resetArmor();
+    void setExperience(int n);
+    int getExperience();
 }
